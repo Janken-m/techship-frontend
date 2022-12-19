ea39a0132135d36d85e1dd4b405728d6d3f30530
Janken-m <jankenmatene933@gmail.com>
2022-12-19 14:41:25 +0100

    remove _redirect

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
@@ -16,9 +16,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <ToastContainer />
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
